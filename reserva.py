0580b6cb9903bde88868e24a1a30ef2761b7ce3b
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-04 16:07:15 -0300

    esqueleto reserva

---
+++ reserva.py
@@ -0,0 +1,22 @@
+from cliente import Cliente
+from quarto import Quarto 
+
+class Reserva:
+    def __init__(self, id_reserva, cliente, quarto, data_checkin, data_checkout, preco_total):
+        self.id_reserva = id_reserva
+        self.cliente = Cliente
+        self.quarto = Quarto
+        self.data_checkin = data_checkin
+        self.data_checkout = data_checkout
+        self.preco_total= preco_total = preco_total
+
+
+    def calcular_total(self):
+        pass
+
+    def confirmar_reserva(self):
+        pass
+
+    def cancelar_reserva(self):
+        pass
+       
