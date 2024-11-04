108542cacc46e82b3c2af165f277ed26179528e7
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-04 16:23:19 -0300

    esqueleto relatorio

---
+++ relatorio.py
@@ -0,0 +1,10 @@
+class Relatorio:
+    def __init__(self, id_relatorio, tipo_relatorio, data_geracao, dados):
+     self.id_relatorio = id_relatorio
+     self.tipo_relatorio = id_relatorio
+     self.data_geracao = data_geracao
+     self.dados = dados
+
+
+    def gerar_relatorio():
+       pass
