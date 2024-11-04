98b4d7d6afd1e9995f2306160ffd1a8d28b29b15
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-04 16:16:19 -0300

    esqueleto pagamento

---
+++ pagamento.py
@@ -0,0 +1,13 @@
+from reserva import Reserva
+
+class Pagameto:
+
+    def __init__(self, id_pagamento, reserva, valor, data_pagamento):
+        self_id_pagamento= id_pagamento
+        reserva = Reserva
+        valor = valor
+        self.data_pagamento = data_pagamento
+
+
+    def processar_pagamento(self):
+        pass
