7044bb59d2f7ce60152183ed18aff88f102265f7
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-04 15:28:23 -0300

    esqueleto adm

---
+++ administrador.py
@@ -0,0 +1,35 @@
+from usuario import Usuario
+from hotel import Hotel
+
+class Administrador(Usuario):
+    def __init__(self, nome_usuario, senha, nome, email, telefone, id_adm):
+        super().__init__(nome_usuario, senha, nome, email, telefone)
+        self.id_adm = id_adm
+
+    def adicionar_quarto(self):
+        pass
+
+    def atualizar_quarto (self):
+        pass
+    
+    def remover_quarto(self):
+        pass
+
+    def visualizar_todas_as_reservas(self):
+        pass
+
+    def gerar_relatorio(self):
+        pass
+
+
+
+
+
+    
+
+
+    
+
+
+ 
+         
