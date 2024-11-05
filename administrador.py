c9018231dcacdd2c603006b6d61570a6bb8e9e43
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-05 15:15:17 -0300

    os add

--- administrador.py
+++ administrador.py
@@ -1,21 +1,29 @@
 from usuario import Usuario
+from quarto import Quarto
+from hotel import Hotel
 
 class Administrador(Usuario):
     def __init__(self, nome_usuario, senha, nome, email, telefone, id_adm):
         super().__init__(nome_usuario, senha, nome, email, telefone)
         self.id_adm = id_adm
 
-    def adicionar_quarto(self):
-        pass
+    def adicionarCliente(self):
+        Hotel.adicionar(Usuario) 
 
-    def atualizar_quarto (self):
-        pass
+    def adicionar_quarto(self):
+        Hotel.addQuarto.append(Quarto)
+
+        
+
+    def setatualizar_quarto (self):
+        Quarto.setDados(input(""))
     
     def remover_quarto(self):
         pass
 
     def visualizar_todas_as_reservas(self):
-        pass
+        for reserrva in reservas:
+            pass
 
     def gerar_relatorio(self):
         pass
