f72aa533eaebf363355cd696ee749d9e2297d95d
emanuelly <emanuellymacahdo777@gmail.com>
2024-11-07 11:02:32 -0300

    tela inicial

--- administrador.py
+++ administrador.py
@@ -1,6 +1,4 @@
 from usuario import Usuario
-from quarto import Quarto
-from hotel import Hotel
 
 class Administrador(Usuario):
     def __init__(self, nome_usuario, senha, nome, email, telefone, id_adm):
@@ -12,27 +10,54 @@ class Administrador(Usuario):
     def getClientes(self):
         for clientes in self.clientes:
             print(f'{Usuario.getNome_usuario()}')
+    
+    @staticmethod
+    def mostrar_opcoes_adm(self):
+       print('Gerenciamento do Hotel')
+       print('Digite o numero da opçao desejada.')
+       print('1 - Adicionar Cliente')
+       print('2 - Adiionar Quarto')
+       print('3 - Atualizar Quarto')
+       print('4 - Remover Quarto')
+       print('5 - Visuallizar Reservas')
+       print('6 - Gerar Relatorio Mensal')
+       oppcao = input('Digite aqui: ')
+
+
+       if oppcao == '1':
+           self.sd
+       elif oppcao == '2':
+          self.adicionar_quarto()
+       elif oppcao == '3':
+          self.atualizar_quarto()
+       elif oppcao == '4':
+          self.remover_quarto()
+       elif oppcao == '5':
+          self.visualizar_todas_as_reservas()
+       elif oppcao == '6':
+          self.gerar_relatorio()
+       else:
+          print('Opção invalida.')
 
     def adicionarCliente(self):
         self.clientes.append(Usuario.getNome_usuario())
         
 
     def adicionar_quarto(self):
-        self.quartos.append(Quarto.getNumero_quarto())
+       pass
 
         
 
     def atualizar_quarto (self):
-        Quarto.setDados(input(""))
+      pass
     
     def remover_quarto(self):
-        self.quartos.remove(Quarto.getNumero_quarto())
+        pass
 
         
 
     def visualizar_todas_as_reservas(self):
-        for reserrva in reservas:
-            pass
+        pass
 
     def gerar_relatorio(self):
         pass
